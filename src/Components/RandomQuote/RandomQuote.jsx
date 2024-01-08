648442d472b203c1a53eab101d321bf4ae24e6a6
acz19 <acz1992@hotmail.co.uk>
2024-01-08 17:16:35 +0100

    Add multiple API functionality

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -1,66 +1,121 @@
-import React, { useState } from "react";
-import "./RandomQuote.css";
-import twitter from "../Assets/twitter.svg";
+import React, { useState, useEffect } from "react";
 import rotate from "../Assets/rotate.svg";
-import { customQuoteSources } from "./DataSources/customQuoteSources";
-import { trumpDataSource } from "./DataSources/trumpDataSource";
+import twitter from "../Assets/twitter.svg";
 import { typeFitDataSource } from "./DataSources/typeFitDataSource";
+import { trumpDataSource } from "./DataSources/trumpDataSource";
+import { customQuoteSources } from "./DataSources/customQuoteSources";
+import "./RandomQuote.css";
 
 export const RandomQuote = () => {
-	let quotes = [];
+	const [quote, setQuote] = useState({});
+	const [loading, setLoading] = useState(false);
 
-	async function loadQuotes() {
-		const response = await fetch("https://type.fit/api/quotes");
-		quotes = await response.json();
-		console.log(quotes);
-	}
+	const getRandomDataSource = () => {
+		const dataSource = [
+			typeFitDataSource,
+			trumpDataSource,
+			customQuoteSources,
+		][Math.floor(Math.random() * 3)];
 
-	const random = () => {
-		const select = quotes[Math.floor(Math.random() * quotes.length)];
-		setQuote(select);
+		if (
+			(dataSource === trumpDataSource ||
+				dataSource === typeFitDataSource) &&
+			!navigator.onLine
+		) {
+			console.error("Internet connection is not available.");
+			return customQuoteSources;
+		}
+
+		return dataSource;
 	};
 
-	const [quote, setQuote] = useState({
-		text: "Difficulties increase the nearer we get to the goal",
-		author: "Johann Wolfgang von Goethe",
-	});
+	const getRandomQuote = async (source) => {
+		const dataSource = source || getRandomDataSource();
+
+		try {
+			const quote = await dataSource.getQuote();
+
+			if (!quote || !quote.quote || !quote.author) {
+				throw new Error("Invalid quote structure");
+			}
+
+			return { ...quote, source: dataSource.name };
+		} catch (error) {
+			throw new Error("Error fetching quote from data source");
+		}
+	};
+
+	const updatePageWithQuote = async () => {
+		setLoading(true);
+
+		let randomQuoteResult;
+		try {
+			randomQuoteResult = await getRandomQuote();
+		} catch (error) {
+			console.log(error);
+			randomQuoteResult = await getRandomDataSource(customQuoteSources);
+		}
+
+		const { quote, author = "Unknown", source } = randomQuoteResult;
+
+		setQuote({ quote, author, source });
+		setLoading(false);
+	};
 
 	const tweetQuote = () => {
 		window.open(
-			`https://twitter.com/intent/tweet?text=${
-				quote.text
-			} - ${quote.author.split(",")[0].trim()}`
+			`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+				`"${quote.quote}"\n- ${quote.author}\n\nSource: ${quote.source}`
+			)}`
 		);
 	};
 
-	loadQuotes();
+	/* const generateRandomColourNumber = () => Math.floor(Math.random() * 255);
+
+	const generateRandomColourCode = () => {
+		const r = generateRandomColourNumber();
+		const g = generateRandomColourNumber();
+		const b = generateRandomColourNumber();
+
+		return { r, g, b };
+	};
+
+	const setRandomBackgroundColour = () => {
+		const { r, g, b } = generateRandomColourCode();
+		document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+	}; */
+
+	useEffect(() => {
+		updatePageWithQuote();
+	}, []);
+
 	return (
 		<div className="container">
-			<div className="quote">{quote.text}</div>
-			<div>
-				<div className="line"></div>
-				<div className="bottom">
-					<div className="author">
-						- {quote.author.split(",")[0].trim()}
-					</div>
-					<div className="icons">
-						<img
-							onClick={() => {
-								random();
-							}}
-							src={rotate}
-							alt=""
-						/>
-						<img
-							src={twitter}
-							onClick={() => {
-								tweetQuote();
-							}}
-							alt=""
-						/>
+			{loading ? (
+				<div className="loading">Loading...</div>
+			) : (
+				<div>
+					<div className="quote">{quote.quote}</div>
+					<div>
+						<div className="line"></div>
+						<div className="bottom">
+							<div className="author">- {quote.author}</div>
+							<div className="icons">
+								<img
+									onClick={updatePageWithQuote}
+									src={rotate}
+									alt=""
+								/>
+								<img
+									onClick={tweetQuote}
+									src={twitter}
+									alt=""
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
